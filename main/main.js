a0307396cb99dd3f70f58f5c7b39378b6533c9ea
Parviz_8901 <parvizakmalov39@gmail.com>
2024-03-12 00:32:06 +0500

    Lesson Js Home || 8

---
+++ main/main.js
@@ -0,0 +1,29 @@
+let a = 100
+let p = prompt('100dan katta son')
+
+
+if (p >= a) {
+    let e = alert('Good!')
+} else {
+    let pp = prompt('100dan katta son')
+    if (pp >= a) {
+        let rr = alert('Good')
+    } else {
+        let op = prompt('100dan katta son')
+        if (op >= a) {
+            let lr = alert('Good')
+        } else {
+            let opp = prompt('100dan katta son')
+            if (opp >= a) {
+                let hr = alert('Good')
+            } else {
+                let opm = prompt('100dan katta son')
+                if (opm >= a) {
+                    let ddd = alert('Good!')
+                } else {
+                    let ehs = alert('Ortiqcha urinish')
+                }
+            }
+        }
+    }
+}
